216edff71c23a62a96cf81e3b487db2d25ee8ab7
Evgeniya <ev.chernova@icloud.com>
2022-12-02 22:12:59 +0300

    Solution for tasks 9,11,12,14,16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new System.Random().Next(100,1000);
+
+Console.WriteLine(num);
+
+//int firstNum = num/100;
+//int secondNum = num%10;
+
+//int result = firstNum*10+secondNum;
+
+int result = (num/100)*10+num%10;
+
+Console.WriteLine(result);
+Console.WriteLine((num/100)*10+num%10);
---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,57 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно число квадратом другого
+//
+//Console.WriteLine("является ли одно число квадратом другого ?");
+//Console.Write("Введите первое число: ");
+//int firstNumber;
+//bool isNumber = int.TryParse(Console.ReadLine(), out firstNumber);
+//validate(isNumber);
+//
+//Console.Write("Введите второе число: ");
+//int secondNumber;
+//isNumber = int.TryParse(Console.ReadLine(), out secondNumber);
+//validate(isNumber);
+//
+//Console.WriteLine(isSqrt(firstNumber,secondNumber)? "Да" : "Нет");
+//
+//void validate(bool isNumber){
+//    if (!isNumber){
+//        Console.WriteLine("Вы ввели не число, выход");
+//        Environment.Exit(0);
+//    }
+//}
+//
+//bool isSqrt(int a, int b){
+//    return (a*a == b) || (b*b == a) ;
+//}
+//
+//
+//
+//
+////Вариант 2
+//int number1 = int.Parse(Console.ReadLine()??"0");
+////Console.WriteLine(number1);
+//
+//int number2 = int.Parse(Console.ReadLine()??"0");
+////Console.WriteLine(number2);
+//
+//if ((int)Math.Pow(number1,2) == number2  | (int)Math.Pow(number2,2) == number1)
+//{
+//    Console.WriteLine("Является");
+//}
+//else
+//{
+//    Console.WriteLine("Не является");
+//}
+
+//
+void TestSQRT(int i, int j)
+{
+bool result = (i/j==j);
+if (result==true)Console.WriteLine(" Число "+i+" является квадратом числа "+j);
+else Console.WriteLine(" Число "+i+" не является квадратом числа"+j);
+}
+int number1 = int. Parse (Console. ReadLine ()??"g");
+int number2 = int. Parse (Console. ReadLine ()??"g");
+TestSQRT (number1, number2) ;
+TestSQRT (number2, number1);
