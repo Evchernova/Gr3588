bdc2cb5cbc41abe8417cba368e2fb9c93dd18695
Evgeniya Chernova <evg@MacBook-Air.local>
2022-12-09 01:05:36 +0300

    Solutions for tasks 19,21,23 added

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = ReadData("Введите число: ");
+bool result = PalinTest(num);
+PrintData(result);
+
+//Метод читает данные от пользователя
+ int ReadData(string msg)
+ {
+     Console.WriteLine(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
+
+// Метод вывода данных
+void PrintData(bool res)
+{
+    Console.WriteLine(res);
+ }
+
+//Метод проверки на палиндром
+bool PalinTest(int num)
+{
+    bool res = false;
+    res = (num/10000 == num%10) && ((num/1000)%10 == (num/10)%10);
+    return res;
+}
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadData(string msg)
+ {
+     Console.WriteLine(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
+
+  int xh = ReadData("Введите xh: ");
+  int yh = ReadData("Введите yh: ");
+  int zh = ReadData("Введите zh: ");
+  int xv = ReadData("Введите xv: ");
+  int yv = ReadData("Введите yv: ");
+  int zv = ReadData("Введите zv: ");
+
+ double CalculateLength(int x1, int x2, int y1, int y2, int z1, int z2)
+ {
+     double res = 0;
+     res = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+     return res;
+ }
+
+
+ void PrintData(string msg, double result)
+ {
+     Console.WriteLine(msg + result);
+ }
+
+ PrintData("Расстояние между точками ", CalculateLength(xh, xv, yh, yv, zh, zv));
